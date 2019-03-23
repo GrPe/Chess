94dfede153e1bb99dafb37b763df49a9fbbd7aa3
Grzegorz Perzanowski <grzesiek.p9804@gmail.com>
2019-03-23 19:09:07 +0100

    Added movement to Rook

--- Assets/Scripts/Pawns/Rook.cs
+++ Assets/Scripts/Pawns/Rook.cs
@@ -6,6 +6,53 @@ public class Rook : Pawn
 {
     public override bool[,] GetPossibleMoves(Pawn[,] pawns)
     {
-        throw new System.NotImplementedException();
+        bool[,] possibleMoves = new bool[8, 8];
+
+        //forward
+        for(int i = XPositionOnBoard - 1; i >=0; i--)
+        {
+            if (pawns[i, YPositionOnBoard] == null) possibleMoves[i, YPositionOnBoard] = true;
+            else
+            {
+                if (pawns[i, YPositionOnBoard].isWhite != isWhite) possibleMoves[i, YPositionOnBoard] = true;
+                break;
+            }
+        }
+
+        //backward
+        for (int i = XPositionOnBoard + 1; i < 8; i++)
+        {
+            if (pawns[i, YPositionOnBoard] == null) possibleMoves[i, YPositionOnBoard] = true;
+            else
+            {
+                if (pawns[i, YPositionOnBoard].isWhite != isWhite) possibleMoves[i, YPositionOnBoard] = true;
+                break;
+            }
+        }
+
+        //left
+        for (int i = YPositionOnBoard - 1; i >= 0; i--)
+        {
+            if (pawns[XPositionOnBoard, i] == null) possibleMoves[XPositionOnBoard, i] = true;
+            else
+            {
+                if (pawns[XPositionOnBoard, i].isWhite != isWhite) possibleMoves[XPositionOnBoard, i] = true;
+                break;
+            }
+        }
+
+        //right
+        for (int i = YPositionOnBoard + 1; i < 8; i++)
+        {
+            if (pawns[XPositionOnBoard, i] == null) possibleMoves[XPositionOnBoard, i] = true;
+            else
+            {
+                if (pawns[XPositionOnBoard, i].isWhite != isWhite) possibleMoves[XPositionOnBoard, i] = true;
+                break;
+            }
+        }
+
+        return possibleMoves;
+
     }
 }
