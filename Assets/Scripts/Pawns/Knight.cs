1cea1d288059d15bbce7d3c54a7b5b2e9c334b6a
Grzegorz Perzanowski <grzesiek.p9804@gmail.com>
2019-03-23 19:56:11 +0100

    Added movement to Knight

--- Assets/Scripts/Pawns/Knight.cs
+++ Assets/Scripts/Pawns/Knight.cs
@@ -6,6 +6,100 @@ public class Knight : Pawn
 {
     public override bool[,] GetPossibleMoves(Pawn[,] pawns)
     {
-        throw new System.NotImplementedException();
+        bool[,] possibleMoves = new bool[8, 8];
+
+        //top-left
+        int x = XPositionOnBoard - 2;
+        int y = YPositionOnBoard - 1;
+        if(x >= 0 && y >= 0)
+        {
+            if (pawns[x, y] == null) possibleMoves[x, y] = true;
+            else if(pawns[x, y].isWhite != isWhite)
+            {
+                possibleMoves[x, y] = true;
+            }
+        }
+
+        x = XPositionOnBoard - 1;
+        y = YPositionOnBoard - 2;
+        if (x >= 0 && y >= 0)
+        {
+            if (pawns[x, y] == null) possibleMoves[x, y] = true;
+            else if (pawns[x, y].isWhite != isWhite)
+            {
+                possibleMoves[x, y] = true;
+            }
+        }
+
+        //top-right
+        x = XPositionOnBoard + 2;
+        y = YPositionOnBoard - 1;
+        if (x < 8 && y >= 0)
+        {
+            if (pawns[x, y] == null) possibleMoves[x, y] = true;
+            else if (pawns[x, y].isWhite != isWhite)
+            {
+                possibleMoves[x, y] = true;
+            }
+        }
+
+        x = XPositionOnBoard + 1;
+        y = YPositionOnBoard - 2;
+        if (x < 8 && y >= 0)
+        {
+            if (pawns[x, y] == null) possibleMoves[x, y] = true;
+            else if (pawns[x, y].isWhite != isWhite)
+            {
+                possibleMoves[x, y] = true;
+            }
+        }
+
+        //down-left
+        x = XPositionOnBoard - 2;
+        y = YPositionOnBoard + 1;
+        if (x >= 0 && y < 8)
+        {
+            if (pawns[x, y] == null) possibleMoves[x, y] = true;
+            else if (pawns[x, y].isWhite != isWhite)
+            {
+                possibleMoves[x, y] = true;
+            }
+        }
+
+        x = XPositionOnBoard - 1;
+        y = YPositionOnBoard + 2;
+        if (x >= 0 && y < 8)
+        {
+            if (pawns[x, y] == null) possibleMoves[x, y] = true;
+            else if (pawns[x, y].isWhite != isWhite)
+            {
+                possibleMoves[x, y] = true;
+            }
+        }
+
+        //down-right
+        x = XPositionOnBoard + 2;
+        y = YPositionOnBoard + 1;
+        if (x < 8 && y < 8)
+        {
+            if (pawns[x, y] == null) possibleMoves[x, y] = true;
+            else if (pawns[x, y].isWhite != isWhite)
+            {
+                possibleMoves[x, y] = true;
+            }
+        }
+
+        x = XPositionOnBoard + 1;
+        y = YPositionOnBoard + 2;
+        if (x < 8 && y < 8)
+        {
+            if (pawns[x, y] == null) possibleMoves[x, y] = true;
+            else if (pawns[x, y].isWhite != isWhite)
+            {
+                possibleMoves[x, y] = true;
+            }
+        }
+
+        return possibleMoves;
     }
 }
