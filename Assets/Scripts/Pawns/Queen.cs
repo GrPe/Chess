1b04a9834b0da14a9efabb24b77f5041d00b92c8
Grzegorz Perzanowski <grzesiek.p9804@gmail.com>
2019-03-23 19:24:24 +0100

    Added movement to Queen

--- Assets/Scripts/Pawns/Queen.cs
+++ Assets/Scripts/Pawns/Queen.cs
@@ -6,6 +6,141 @@ public class Queen : Pawn
 {
     public override bool[,] GetPossibleMoves(Pawn[,] pawns)
     {
-        throw new System.NotImplementedException();
+        bool[,] possibleMoves = new bool[8, 8];
+
+        #region King
+        int i = XPositionOnBoard - 1;
+        int j = YPositionOnBoard - 1;
+
+        for (; i <= XPositionOnBoard + 1; i++)
+        {
+            if (i > 7 || i < 0) continue;
+            for (j = YPositionOnBoard - 1; j <= YPositionOnBoard + 1; j++)
+            {
+                if (j > 7 || j < 0) continue;
+                if (pawns[i, j] == null)
+                {
+                    possibleMoves[i, j] = true;
+                }
+                else
+                {
+                    if (pawns[i, j].isWhite != isWhite)
+                    {
+                        possibleMoves[i, j] = true;
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region Bishop
+        //left-up
+        for (i = XPositionOnBoard - 1, j = YPositionOnBoard - 1; i >= 0 && j >= 0; i--, j--)
+        {
+            if (pawns[i, j] == null) possibleMoves[i, j] = true;
+            else
+            {
+                if (pawns[i, j].isWhite != isWhite)
+                {
+                    possibleMoves[i, j] = true;
+                    break;
+                }
+                else break;
+            }
+        }
+
+        //right-up
+        for (i = XPositionOnBoard + 1, j = YPositionOnBoard - 1; i < 8 && j >= 0; i++, j--)
+        {
+            if (pawns[i, j] == null) possibleMoves[i, j] = true;
+            else
+            {
+                if (pawns[i, j].isWhite != isWhite)
+                {
+                    possibleMoves[i, j] = true;
+                    break;
+                }
+                else break;
+            }
+        }
+
+        //left-down
+        for (i = XPositionOnBoard - 1, j = YPositionOnBoard + 1; i >= 0 && j < 8; i--, j++)
+        {
+            if (pawns[i, j] == null) possibleMoves[i, j] = true;
+            else
+            {
+                if (pawns[i, j].isWhite != isWhite)
+                {
+                    possibleMoves[i, j] = true;
+                    break;
+                }
+                else break;
+            }
+        }
+
+        //right-down
+        for (i = XPositionOnBoard + 1, j = YPositionOnBoard + 1; i < 8 && j < 8; i++, j++)
+        {
+            if (pawns[i, j] == null) possibleMoves[i, j] = true;
+            else
+            {
+                if (pawns[i, j].isWhite != isWhite)
+                {
+                    possibleMoves[i, j] = true;
+                    break;
+                }
+                else break;
+            }
+        }
+        #endregion
+
+        #region Rook
+        //forward
+        for (i = XPositionOnBoard - 1; i >= 0; i--)
+        {
+            if (pawns[i, YPositionOnBoard] == null) possibleMoves[i, YPositionOnBoard] = true;
+            else
+            {
+                if (pawns[i, YPositionOnBoard].isWhite != isWhite) possibleMoves[i, YPositionOnBoard] = true;
+                break;
+            }
+        }
+
+        //backward
+        for (i = XPositionOnBoard + 1; i < 8; i++)
+        {
+            if (pawns[i, YPositionOnBoard] == null) possibleMoves[i, YPositionOnBoard] = true;
+            else
+            {
+                if (pawns[i, YPositionOnBoard].isWhite != isWhite) possibleMoves[i, YPositionOnBoard] = true;
+                break;
+            }
+        }
+
+        //left
+        for (i = YPositionOnBoard - 1; i >= 0; i--)
+        {
+            if (pawns[XPositionOnBoard, i] == null) possibleMoves[XPositionOnBoard, i] = true;
+            else
+            {
+                if (pawns[XPositionOnBoard, i].isWhite != isWhite) possibleMoves[XPositionOnBoard, i] = true;
+                break;
+            }
+        }
+
+        //right
+        for (i = YPositionOnBoard + 1; i < 8; i++)
+        {
+            if (pawns[XPositionOnBoard, i] == null) possibleMoves[XPositionOnBoard, i] = true;
+            else
+            {
+                if (pawns[XPositionOnBoard, i].isWhite != isWhite) possibleMoves[XPositionOnBoard, i] = true;
+                break;
+            }
+        }
+        #endregion
+
+        return possibleMoves;
     }
 }
