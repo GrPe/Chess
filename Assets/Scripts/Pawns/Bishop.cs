72e4a6b627fe06bc4bea46bf0beb5d785e3b7fbb
Grzegorz Perzanowski <grzesiek.p9804@gmail.com>
2019-03-23 19:18:09 +0100

    Added movement to Bishop

--- Assets/Scripts/Pawns/Bishop.cs
+++ Assets/Scripts/Pawns/Bishop.cs
@@ -6,6 +6,69 @@ public class Bishop : Pawn
 {
     public override bool[,] GetPossibleMoves(Pawn[,] pawns)
     {
-        throw new System.NotImplementedException();
+        bool[,] possibleMoves = new bool[8, 8];
+
+        //left-up
+        int i, j;
+        for (i = XPositionOnBoard - 1, j = YPositionOnBoard - 1; i >= 0 && j >= 0; i--, j--)
+        {
+            if (pawns[i, j] == null) possibleMoves[i, j] = true;
+            else
+            {
+                if (pawns[i, j].isWhite != isWhite)
+                {
+                    possibleMoves[i, j] = true;
+                    break;
+                }
+                else break;
+            }
+        }
+
+        //right-up
+        for (i = XPositionOnBoard + 1, j = YPositionOnBoard - 1; i < 8 && j >= 0; i++, j--)
+        {
+            if (pawns[i, j] == null) possibleMoves[i, j] = true;
+            else
+            {
+                if (pawns[i, j].isWhite != isWhite)
+                {
+                    possibleMoves[i, j] = true;
+                    break;
+                }
+                else break;
+            }
+        }
+
+        //left-down
+        for (i = XPositionOnBoard - 1, j = YPositionOnBoard + 1; i >= 0 && j < 8; i--, j++)
+        {
+            if (pawns[i, j] == null) possibleMoves[i, j] = true;
+            else
+            {
+                if (pawns[i, j].isWhite != isWhite)
+                {
+                    possibleMoves[i, j] = true;
+                    break;
+                }
+                else break;
+            }
+        }
+
+        //right-down
+        for (i = XPositionOnBoard + 1, j = YPositionOnBoard + 1; i < 8 && j < 8; i++, j++)
+        {
+            if (pawns[i, j] == null) possibleMoves[i, j] = true;
+            else
+            {
+                if (pawns[i, j].isWhite != isWhite)
+                {
+                    possibleMoves[i, j] = true;
+                    break;
+                }
+                else break;
+            }
+        }
+
+        return possibleMoves;
     }
 }
