b40fc56597f005094df57a45cecf02017dad7f89
Grzegorz Perzanowski <grzesiek.p9804@gmail.com>
2019-03-26 13:13:29 +0100

    Added main menu

---
+++ Assets/Menu/MenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void OnPvPClick()
+    {
+        Config.isMultiplayer = false;
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnAIClick()
+    {
+        Config.isMultiplayer = true;
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnExit()
+    {
+        Application.Quit();
+    }
+}
